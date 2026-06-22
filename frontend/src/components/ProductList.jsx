0b078c563b817b2b746448c06141b820aca95c58
Pawan <154042595+This-is-pawan@users.noreply.github.com>
2026-06-22 21:01:42 +0530

    Update ProductList.jsx

--- frontend/src/components/ProductList.jsx
+++ frontend/src/components/ProductList.jsx
@@ -113,7 +113,7 @@ setPage(1);
         </div>
       )}
 {/* pagination */}
-      {data.length>0:
+      {data.length>0 ? :
 <div className="flex justify-center items-center gap-4 mt-8">
   <button
     disabled={page === 1}
@@ -134,7 +134,7 @@ setPage(1);
   >
     Next
   </button>
-</div>:""
+</div>: ""
       }
 
       {/* Empty State */}
